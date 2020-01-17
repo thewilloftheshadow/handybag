c66773fcb0113619855f063cf0df907197dad5d9
Shadow <william.shadow@outlook.com>
2020-01-17 11:01:06 +0000

    Add prettier

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const Discord = require("discord.js")
-const giveaways = require("discord-giveaways")
-const package = require("./package.json")
+const Discord = require("discord.js");
+const giveaways = require("discord-giveaways");
+const package = require("./package.json");
 
 module.exports = {
-giveaways: giveaways,
-version: package.version,
-mutualservers: function(userid){
-let mutualservers = [];
-  client.guilds.forEach(guild => {
-    let guildobj = { name: guild.name, id: guild.id };
-    let guildmember = guild.members.get(userid);
-    if (guildmember) {
-      mutualservers.push(guildobj);
-    }
-  });
-  if (mutualservers === []) return false;
-  return mutualservers;
-}
-}
+  giveaways: giveaways,
+  version: package.version,
+  mutualservers: function(userid) {
+    let mutualservers = [];
+    client.guilds.forEach(guild => {
+      let guildobj = { name: guild.name, id: guild.id };
+      let guildmember = guild.members.get(userid);
+      if (guildmember) {
+        mutualservers.push(guildobj);
+      }
+    });
+    if (mutualservers === []) return false;
+    return mutualservers;
+  }
+};
