adedcf859bfb36ead58bc906ad9afee88e313b50
Shadow <william.shadow@outlook.com>
2020-01-17 10:34:08 +0000

    Add mutualservers()

--- index.js
+++ index.js
@@ -5,4 +5,16 @@ const package = require("./package.json")
 module.exports = {
 giveaways: giveaways,
 version: package.version
+mutualservers: function(userid){
+let mutualservers = [];
+  client.guilds.forEach(guild => {
+    let guildobj = { name: guild.name, id: guild.id };
+    let guildmember = guild.members.get(userid);
+    if (guildmember) {
+      mutualservers.push(guildobj);
+    }
+  });
+  if (mutualservers === []) return false;
+  return mutualservers;
+}
 }
