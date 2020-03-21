0c20006f8718ac01b71bf45c06a3468d3fec0047
Node Code Formatter <formatter@1337z.ninja>
2020-03-21 20:04:14 +0000

    Formatted code

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const package = require("./package.json");
 
 module.exports = {
   version: package.version,
-  mutualservers: function(userid) {
+  mutualservers: function (userid) {
     let mutualservers = [];
-    client.guilds.forEach(guild => {
+    client.guilds.forEach((guild) => {
       let guildobj = { name: guild.name, id: guild.id };
       let guildmember = guild.members.get(userid);
       if (guildmember) {
@@ -15,9 +15,9 @@ module.exports = {
     if (mutualservers === []) return false;
     return mutualservers;
   },
-  glitchinvite: function() {
+  glitchinvite: function () {
     if (process.env.PROJECT_INVITE_TOKEN)
       return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
     return false;
-  }
+  },
 };
