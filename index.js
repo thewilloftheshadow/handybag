d0906cec8ed43944a013d1721874399d76c0a144
Node Code Formatter <formatter@1337z.ninja>
2020-04-12 22:48:30 +0000

    Formatted code

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const package = require("./package.json");
  * @param {string} userid - The id of the user you want to search
  */
 const mutualservers = (client, userid) => {
-    let mutualservers = [];
-    client.guilds.cache.forEach(guild => {
-      let guildobj = { name: guild.name, id: guild.id };
-      let guildmember = guild.members.cache.get(userid);
-      if (guildmember) {
-        mutualservers.push(guildobj);
-      }
-    });
-    if (mutualservers === []) return false;
-    return mutualservers;
-  }
+  let mutualservers = [];
+  client.guilds.cache.forEach(guild => {
+    let guildobj = { name: guild.name, id: guild.id };
+    let guildmember = guild.members.cache.get(userid);
+    if (guildmember) {
+      mutualservers.push(guildobj);
+    }
+  });
+  if (mutualservers === []) return false;
+  return mutualservers;
+};
 
 /**
  * Get a project invite from Glitch if there is one
  */
 const glitchinvite = () => {
-    if (process.env.PROJECT_INVITE_TOKEN)
-      return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
-    return false;
-  }
+  if (process.env.PROJECT_INVITE_TOKEN)
+    return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
+  return false;
+};
 
 module.exports = {
   version: package.version,
