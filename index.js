fa7166f85fd5325ea4a5b741316c06c0f904a5bb
thewilloftheshadow <william.shadow@outlook.com>
2020-04-12 17:47:57 -0500

    Add documentation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Discord = require("discord.js");
 const package = require("./package.json");
 
-module.exports = {
-  version: package.version,
-  mutualservers: function(client, userid) {
+/**
+ * Get the mutual servers of the bot and a user
+ * @param {object} client - An instance of Discord.Client for the function to use
+ * @param {string} userid - The id of the user you want to search
+ */
+const mutualservers = (client, userid) => {
     let mutualservers = [];
     client.guilds.cache.forEach(guild => {
       let guildobj = { name: guild.name, id: guild.id };
@@ -14,10 +17,19 @@ module.exports = {
     });
     if (mutualservers === []) return false;
     return mutualservers;
-  },
-  glitchinvite: function() {
+  }
+
+/**
+ * Get a project invite from Glitch if there is one
+ */
+const glitchinvite = () => {
     if (process.env.PROJECT_INVITE_TOKEN)
       return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
     return false;
   }
+
+module.exports = {
+  version: package.version,
+  mutualservers: mutualservers,
+  glitchinvite: glitchinvite
 };
