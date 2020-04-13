c7766068a53633eec310f2660a4bba62044c54f2
thewilloftheshadow <william.shadow@outlook.com>
2020-04-12 21:35:53 -0500

    Add docs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const package = require("./package.json");
+const pkg = require("./package.json");
 
 /**
  * Get the mutual servers of the bot and a user
@@ -7,29 +7,29 @@ const package = require("./package.json");
  * @param {string} userid - The id of the user you want to search
  */
 const mutualservers = (client, userid) => {
-    let mutualservers = [];
-    client.guilds.cache.forEach(guild => {
-      let guildobj = { name: guild.name, id: guild.id };
-      let guildmember = guild.members.cache.get(userid);
-      if (guildmember) {
-        mutualservers.push(guildobj);
-      }
-    });
-    if (mutualservers === []) return false;
-    return mutualservers;
-  }
+  let mutualservers = [];
+  client.guilds.cache.forEach(guild => {
+    let guildobj = { name: guild.name, id: guild.id };
+    let guildmember = guild.members.cache.get(userid);
+    if (guildmember) {
+      mutualservers.push(guildobj);
+    }
+  });
+  if (mutualservers === []) return false;
+  return mutualservers;
+};
 
 /**
  * Get a project invite from Glitch if there is one
  */
 const glitchinvite = () => {
-    if (process.env.PROJECT_INVITE_TOKEN)
-      return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
-    return false;
-  }
+  if (process.env.PROJECT_INVITE_TOKEN)
+    return `https://glitch.com/edit/#!/join/${process.env.PROJECT_INVITE_TOKEN}`;
+  return false;
+};
 
 module.exports = {
-  version: package.version,
+  version: pkg.version,
   mutualservers: mutualservers,
   glitchinvite: glitchinvite
 };
